93b5483e5d4bc7b35d5a659141f98c27af4b1353
Plus-y-j-w <77605316+Plus-y-j-w@users.noreply.github.com>
2026-08-31 14:54:47 -0600

    Add database abstraction layer

---
+++ src/database/db.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+
+class Database:
+    """Zen database abstraction.
+
+    Later versions will support Cloudflare D1 and SQLite.
+    """
+
+    def __init__(self):
+        self.connected = False
+
+    async def connect(self):
+        self.connected = True
+
+    async def close(self):
+        self.connected = False
+
+
+ database = Database()
