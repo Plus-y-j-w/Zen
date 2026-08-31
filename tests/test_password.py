6cb988d1e2d1f0539ad4097c7858fc170b242b2c
Plus-y-j-w <77605316+Plus-y-j-w@users.noreply.github.com>
2026-08-31 14:58:36 -0600

    Add password security tests

---
+++ tests/test_password.py
@@ -0,0 +1,10 @@
+from src.core.password import hash_password, verify_password
+
+
+def test_password_hash():
+    password = '123456'
+    hashed = hash_password(password)
+
+    assert hashed != password
+    assert verify_password(password, hashed) is True
+    assert verify_password('wrong', hashed) is False
